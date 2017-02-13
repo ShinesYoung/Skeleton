04c9c5b353676f9df7197d666e754017d0ebeab0
Shines Young <yss_1984@163.com>
2017-02-13 14:35:09 +0800

    append subview strategy

---
+++ Skeleton/Router/SKSubviewStrategy.h
@@ -0,0 +1,13 @@
+//
+//  SKSubviewStrategy.h
+//  Skeleton
+//
+//  Created by Shines Young on 2017/2/13.
+//  Copyright © 2017年 IDEA. All rights reserved.
+//
+
+#import "SKBaseStrategy.h"
+
+@interface SKSubviewStrategy : SKBaseStrategy
+
+@end

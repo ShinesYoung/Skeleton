22b8bff586145ddb56ff9d841a484b42cef6adfb
ShinesYoung <yss_1984@163.com>
2017-03-14 10:44:33 +0800

    Create SKStackResetStrategy.h

---
+++ Skeleton/Router/Strategy/SKStackResetStrategy.h
@@ -0,0 +1,15 @@
+//
+//  SKStackResetStrategy.h
+//  Skeleton
+//
+//  Created by WiLL on 2017/3/8.
+//  Copyright © 2017年 IDEA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SKBaseStrategy.h"
+
+@interface SKStackResetStrategy : SKBaseStrategy
+
+@end
